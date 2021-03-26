f3ca3376cd4ec6ad9ca2000ea61c82583edef516
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-26 14:32:14 +0100

    Play around with multiple projects

---
+++ src/Blazor.LibB/Greeter.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor.LibB
+{
+    public static class Greeter
+    {
+        public static string SayHello()
+        {
+            return "Hello from Lib B";
+        }
+        
+        public static string LetCSayHello()
+        {
+            return LibC.Greeter.SayHello();
+        }
+    }
+}
