828c9a364643000e4bc67b4021f6d38f7f150325
Dante De Ruwe <dantederuwe@gmail.com>
2021-04-12 20:52:38 +0200

    Add route param support

--- numbers-pilet/src/index.tsx
+++ numbers-pilet/src/index.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { PiletApi } from 'sample-cross-fx';
+import { PiletApi } from 'demo-piral-blazor-appshell';
 import './style.scss';
+import { Link } from 'react-router-dom';
 
 interface BlazorPages {
   registerBlazorPages: (app: PiletApi) => void;
   blazorRoutes: string[];
+  paths: string[];
 }
 
-//prettier-ignore
-const RouteList: React.FC<{ routes: string[] }> = ({ routes }) => <ul>{routes.map((x, i) => (<li key={i}><Link to={x}>{x}</Link></li>))}</ul>;
+const zip: (args) => any[] = (...rows) => [...rows[0]].map((_, c) => rows.map(row => row[c]));
 
+//prettier-ignore
+const RouteList: React.FC<{ routes: string[], paths: string[] }> = ({ routes, paths }) => <table style={{fontSize: ".7rem"}}><tbody>{zip(routes, paths).map((x, i) => (<tr key={i}><td>{x[0]}</td><td>&#8594;</td><td>{x[1]}</td></tr>))}</tbody></table>;
 const Tile: React.FC<React.PropsWithChildren<any>> = ({ children }) => <div className="tile">{children}</div>;
 
 export function setup(app: PiletApi) {
@@ -26,14 +28,23 @@ export function setup(app: PiletApi) {
   app.registerTile(app.fromBlazor('numbers-tile')); //from blazor
 
   // register Blazor pages
-  const { registerBlazorPages, blazorRoutes }: BlazorPages = require('./pages.codegen');
+  const { registerBlazorPages, blazorRoutes, paths }: BlazorPages = require('./pages.codegen');
   registerBlazorPages(app);
 
   // access to all defined blazor routes
   app.registerTile(() => (
     <Tile>
       <code>Blazor.Numbers</code>
-      <RouteList routes={blazorRoutes} />
+      <br />
+      <RouteList routes={blazorRoutes} paths={paths} />
     </Tile>
   ));
+
+  app.registerTile(() => (
+    <Tile>
+      <Link to={`/bar/baz/${Math.floor(Math.random() * 500)}`}>bar baz</Link>
+    </Tile>
+  ));
+  // app.registerPage('/bar/baz/:id?', p => <p>hallo {p.match.params.id}</p>);
+  app.registerPage('/bar/baz/:id?', app.fromBlazor('rng-blazor'));
 }
